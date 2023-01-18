985abe0cf0c33e2f54f06f77d1c87ba9b8676a48
soaringsso <ksi06191@naver.com>
2023-01-18 16:05:42 +0900

    reupload

--- Ssohee/221219_phone-book.py
+++ Ssohee/221219_phone-book.py
@@ -2,16 +2,14 @@
 # 프로그래머스 Lv2. 전화번호 목록
 
 def solution(phone_book):
-    answer = True
     phone_book.sort()      #정렬하면 각 자리의 수가 적은 것 순으로 
     for i in range(len(phone_book)-1):
         #if phone[i+1].startswith(phone[i]):  
         n = len(phone_book[i])
         if phone_book[i+1][:n] == phone_book[i]:
             #startswith()와 같은 로직 
-            answer = False
-            break
-    return answer
+            return False
+    return True
 
 phone_book = ["123","456","789"]
 print(solution(phone_book))
