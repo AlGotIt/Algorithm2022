c50d7e77b32fffc45d5a67d4509d5ba6a4da0247
Kang Dain <dmelli0505@gmail.com>
2023-02-01 22:55:44 +0900

    230118 Programmers Lv.3 합승 택시 요금 문제풀이

--- Inu/230118_Programmers_합승_택시_요금.py
+++ Inu/230118_Programmers_합승_택시_요금.py
@@ -53,13 +53,10 @@ def solution(n, s, a, b, fares) -> int:
     return answer
 
 
-
-
-# def dijkstra(fares, n):
-    
 n = 6	
 s = 4	
 a = 6	
 b = 2	
 fares = [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]
+
 solution(n, s, a, b, fares)
